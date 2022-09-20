73d575b7bcfdbd53e147ad0e84b548793aef2118
E-Vivanco <vivanco.elsa@gmail.com>
2022-09-20 21:40:22 +0000

    Modificaciones actualizadas

--- src/app.js
+++ src/app.js
@@ -4,8 +4,30 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+let generaExcusa = () => {
+  let quien = [
+    "Mi perro",
+    "El gato",
+    "El auto",
+    "El niño",
+    "Mi Hermano",
+    "Mi canario"
+  ];
+  let accion = ["rompio", "aplasto", "destrozo", "perdio", "borro", "ensucio"];
+  let que = ["mi tarea", "mi Notebook", "mi telefono", "mi pc", "mi llave"];
+  let cuando = ["hoy", "ayer", "la semana pasada", "hace 2 dias", "hace 1hr"];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let uno = parseInt(Math.floor(Math.random() * quien.length));
+  let dos = parseInt(Math.floor(Math.random() * accion.length));
+  let tres = parseInt(Math.floor(Math.random() * que.length));
+  let cuatro = parseInt(Math.floor(Math.random() * cuando.length));
+  return (
+    quien[uno] + " " + accion[dos] + " " + que[tres] + " " + cuando[cuatro]
+  );
 };
+window.onload = function() {
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#My-resp").innerHTML = generaExcusa();
+  });
+};
+//console.log("Hello Rigo from the console!");
---
+++ src/index.js
@@ -0,0 +1,6 @@
+//import {generaExcusa} from './js/componente.js';
+//import { generaExcusa } from './js/app.js';
+import './style.css';
+
+
+//generaExcusa();
